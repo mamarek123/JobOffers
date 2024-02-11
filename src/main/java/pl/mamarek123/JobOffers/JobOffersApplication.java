c36fcf5cd2dc192b3bb29e5d6cbe66af900db8b6
kaczo <kaczoreks121@gmail.com>
2024-02-11 13:01:20 +0100

    Starting the project

---
+++ src/main/java/pl/mamarek123/JobOffers/JobOffersApplication.java
@@ -0,0 +1,13 @@
+package pl.mamarek123.JobOffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOffersApplication.class, args);
+	}
+
+}
