7122712276dbc23685988a15a10ec2dd22ecf290
kaczo <kaczoreks121@gmail.com>
2024-03-13 22:47:47 +0100

    A lot of fixes and changes push

---
+++ src/main/java/pl/mamarek123/JobOffers/infrastructure/offer/controller/OfferController.java
@@ -0,0 +1,42 @@
+package pl.mamarek123.JobOffers.infrastructure.offer.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.mamarek123.JobOffers.domain.offer.DTO.OfferRequestDto;
+import pl.mamarek123.JobOffers.domain.offer.DTO.OfferResponseDto;
+import pl.mamarek123.JobOffers.domain.offer.OfferFacade;
+import springfox.documentation.annotations.Cacheable;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@Log4j2
+public class OfferController {
+
+    private final OfferFacade offerFacade;
+
+    @Cacheable("jobOffers")
+    @GetMapping("/offers")
+    public ResponseEntity<List<OfferResponseDto>> getAllOffers(){
+        List<OfferResponseDto> allOffers = offerFacade.findAllOffers();
+        return ResponseEntity.ok(allOffers);
+    }
+
+    @GetMapping("/offers/{id}")
+    public ResponseEntity<OfferResponseDto> getOfferById(@PathVariable String id){
+        OfferResponseDto offerById = offerFacade.findOfferById(id);
+        return ResponseEntity.ok(offerById);
+    }
+
+    @PostMapping("/offers")
+    public ResponseEntity<OfferResponseDto> addNewOffer(@RequestBody @Valid OfferRequestDto offer) {
+        OfferResponseDto savedOffer = offerFacade.saveOffer(offer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedOffer);
+    }
+
+}
